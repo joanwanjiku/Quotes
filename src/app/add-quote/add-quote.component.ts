73e4ae13a5b3f8a44ab4f7f51b2f048c3518975e
Joan_E <joanevans18@gmail.com>
2020-06-14 14:23:23 +0300

    Added a form with validations

--- src/app/add-quote/add-quote.component.ts
+++ src/app/add-quote/add-quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../models/quote';
 
 @Component({
   selector: 'app-add-quote',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddQuoteComponent implements OnInit {
 
-  constructor() { }
+  newQuote: Quote;
+  constructor() { 
+    this.newQuote = new Quote(0, "", "", "", new Date)
+  }
 
   ngOnInit(): void {
   }
